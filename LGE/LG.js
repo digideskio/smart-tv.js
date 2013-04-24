27668b9ee6888f45846b63fa8b9be5fb0e5851c5
Rui Posse <ruiposse@gmail.com>
2013-04-24 12:26:02 +0100

    LG udpate.

--- LGE/LG.js
+++ LGE/LG.js
@@ -2,46 +2,87 @@ var LG = {
 
 	_device: null,
 	_log: null,
+	logConfig: false,
+	onFocusId: '',
+	onHoverId: '',
 
 	init: function () {
-		var tmp;
-
 		this._device = document.getElementById("deviceObj");
 		if(!this._device) {
-			tmp = document.createElement("div");
+			var tmp = document.createElement("div");
 			tmp.innerHTML = "<object type='application/x-netcast-info' id='deviceObj'> </object>";
 			document.body.appendChild(tmp);
 			this._device = document.getElementById("deviceObj");
 		}
-		
+
 		// init log
-		if(!this._log) {
-			tmp = document.createElement("div");
-			tmp.setAttribute('id', 'debug');
-			tmp.setAttribute('style', 'position: absolute; top: 10px; right: 10px;' +
-				'width: 200px; height: 700px; color: pink; font-size: 14px; ' +
-				'line-height: 18px; border: none; z-index: 3000; display: block;');
-			document.body.appendChild(tmp);
-			this._log = document.getElementById("debug");
-		}
 		this._log = document.getElementById("debug");
+	},
+
+
+	//
+	// focus
+	//
+	getOnFocusId: function () { return this.onFocusId; },
+	setFocus: function (elementID) {
+		var prev = document.getElementById(this.onFocusId),
+			el = document.getElementById(elementID);
+
+		// take focus away from existing focused element
+		if (prev) {
+			prev.blur();
+			this.onFocusId = '';
+		}
+
+		// set focus on new element
+		if (el) {
+			el.focus();
+			this.onFocusId = elementID;
+		}
+	},
+	isFocused: function (otherID) {
+		return otherID == this.onFocusId;
+	},
+
+	//
+	// hover
+	//
+	setHover: function (elementID) {
+		var prev = document.getElementById(this.onHoverId),
+			el = document.getElementById(elementID);
+
+		// take hover away from existing hovered element
+		if (prev) {
+			this.offHover(this.onHoverId);
+		}
+
+		// set hover
+		if (el) {
+			el.className += " hover";
+			this.onHoverId = elementID;
+		}
+	},
+	offHover: function (elementID) {
+		var el = document.getElementById(elementID);
+		el.className = el.className.replace( /(?:^|\s)hover(?!\S)/g , '' );
+		this.hoverEl = '';
+	},
+
+
+
+	qmenu: function () {
+		if(window.NetCastLaunchQMENU)
+			window.NetCastLaunchQMENU();
 	},
 
 	quit: function () {
 		window.NetCastBack();
 	},
 
-	log: function (text) {
-		this._log.innerHTML = text + "<br />" + this._log.innerHTML;
-		console.log(text);
-	},
-
-	setDebugMode: function (mode) {
-		if(mode) {
-			_log.style.visibility = 'hidden';
-		} else {
-			_log.style.visibility = 'visible';
+	log : function (text) {
+		if (this.logConfig) {
+			this._log.innerHTML = text + "<br />" + this._log.innerHTML;
+			console.log(text);
 		}
 	}
-
 };
