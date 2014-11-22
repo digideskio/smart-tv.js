1890608ce48a83a80ca228aba2b5cef8b16f462c
Rui Posse <ruiposse@gmail.com>
2014-11-22 15:13:04 +0000

    [Platform] Updated interface.

--- src/platform.js
+++ src/platform.js
@@ -2,7 +2,7 @@
 //  PLATFORM
 //------------------------------------//
 
-//
+// Interface to be inherited and implemented by all supported platforms.
 
 
 
@@ -12,21 +12,31 @@ var TV_Platform = function(name) {
 
 
 TV_Platform.prototype = {
-
 	//
 	// Initialize platform.
 	//
 	init: function() {},
 
-	getDeviceName: function() {
-		return this.name;
-	},
+	//
+	// Name of the platform.
+	//
+	getPlatformName: function() { return this.name; },
+
+	//
+	// Name of the device's model.
+	//
+	getModel: function() {},
+
+	//
+	// Get the device UUID.
+	// http://en.wikipedia.org/wiki/Universally_unique_identifier
+	//
+	getUUID: function() {},
 
 	//
 	// Close application.
 	//
 	exit: function() {}
-
 };
 
 module.exports = TV_Platform;
