c97244107f32831416d5df9818b70b39e71c6de2
Rui Posse <rui.posse@saffrondigital.com>
2014-12-08 16:00:43 +0000

    [Storage] Fixed identation in default.js.

--- src/storage/default.js
+++ src/storage/default.js
@@ -9,29 +9,29 @@
 var TV_Storage = require('../storage');
 
 var TV_Storage_Default = function() {
-    TV_Storage.apply(this);
+	TV_Storage.apply(this);
 
-    // TV_Storage interface is based on HTML5 localStorage, so
-    // TV_Storage_Default should use that.
+	// TV_Storage interface is based on HTML5 localStorage, so
+	// TV_Storage_Default should just use HTML5 localStorage.
 
-    // http://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
-    var hasStorage = (function() {
-        var test = 'test';
-        try {
-            localStorage.setItem(test, test);
-            localStorage.removeItem(test);
-            return true;
-        } catch (exception) {
-            return false;
-        }
-    }());
+	// http://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
+	var hasStorage = (function() {
+		var test = 'test';
+		try {
+			localStorage.setItem(test, test);
+			localStorage.removeItem(test);
+			return true;
+		} catch (exception) {
+			return false;
+		}
+	}());
 
-    // get localStorage
-    if (hasStorage) {
-        return window.localStorage;
-    } else {
-        return null;
-    }
+	// get localStorage
+	if (hasStorage) {
+		return window.localStorage;
+	} else {
+		return null;
+	}
 };
 
 module.exports = TV_Storage_Default;
