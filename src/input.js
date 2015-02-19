06951a32391e15aa6e32565f76d31fd4a71ca261
Rui Posse <rui.posse@saffrondigital.com>
2015-02-19 17:56:35 +0000

    [Input] Initial commit for TV_Input support.

---
+++ src/input.js
@@ -0,0 +1,123 @@
+//------------------------------------//
+//  INPUT
+//------------------------------------//
+
+// Object to be instanciated by all supported platforms.
+
+
+
+var TV_Input = function(platform) {
+	// Initialize.
+	this.init(platform);
+};
+
+
+TV_Input.prototype = {
+	//
+	// Keys.
+	//
+	key: {},
+
+	//
+	// Status for platform key input.
+	// (Set to false for disabling input)
+	//
+	enabled: true,
+
+	//
+	// Initialize input.
+	//
+	init: function(platform) {
+		// Create key map.
+		this.buildKeyMap(platform);
+
+		// bind keydown ?
+		// document.addEventListener('keydown', function(ev) { console.log(ev); }, false);
+	},
+
+
+	//  Key Maps
+	//------------------------------------//
+
+	keyMap: {
+		VK_LEFT: 37,
+		VK_UP: 38,
+		VK_RIGHT: 39,
+		VK_DOWN: 40,
+		VK_ENTER: 13,
+		VK_BACK: 8
+	},
+
+	//
+	// Create key map based on platform name.
+	//
+	buildKeyMap: function(platform) {
+		console.log('keyMap' + platform);
+		this.key = this['keyMap' + platform]();
+	},
+
+	//
+	// 'Default' Platform key maps.
+	// (usually browsers)
+	//
+	keyMapDefault: function() {
+		// http://www.w3.org/TR/2001/WD-DOM-Level-3-Events-20010410/DOM3-Events.html#events-Events-KeyEvent
+		if (window.KeyEvent && window.KeyEvent.DOM_VK_LEFT) {
+			this.key = this.createKeys('DOM_VK_', window.KeyEvent);
+			// Normalize VK_ENTER and VK_BACK
+			this.key.VK_ENTER = this.key.VK_RETURN;
+			this.key.VK_BACK = this.key.VK_BACK_SPACE;
+		} else {
+			// Absolute default keys.
+			// http://www.javascripter.net/faq/keycodes2011.htm
+			return this.createKeys('VK_', this.keyMap);
+		}
+	},
+
+	//
+	// 'Samsung' Platform key maps.
+	//
+	keyMapSamsung: function() {
+		this.key = this.createKeys('KEY_', new Common.API.TVKeyValue());
+		// Normalize VK_BACK
+		this.key.VK_BACK = this.key.VK_RETURN;
+	},
+
+	//
+	// Map virtual keys.
+	//
+	createKeys: function(prefix, platformKeys) {
+		var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+			'O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+			k, i = 0, j = 0, tmp,
+			result = {};
+
+		// copy special keys
+		for (k in platformKeys) {
+			if (platformKeys.hasOwnProperty(k)) {
+				tmp = k.replace(prefix, '');
+				result['VK_' + tmp] = platformKeys[k];
+			}
+		}
+		// copy number keys
+		for (i = 0, j = 48; i <= 9; i++, j++) {
+			if (typeof platformKeys[prefix + i] !== 'undefined') {
+				result['VK_' + i] = platformKeys[prefix + i];
+			} else {
+				result['VK_' + i] = j;
+			}
+		}
+		// copy letters
+		for (i = 0, j = 65; i < alpha.length; i++, j++) {
+			if (typeof platformKeys[prefix + alpha[i]] !== 'undefined') {
+				result['VK_' + alpha[i]] = platformKeys[prefix + alpha[i]];
+			} else {
+				result['VK_' + alpha[i]] = j;
+			}
+		}
+		return result;
+	}
+
+};
+
+module.exports = TV_Input;
